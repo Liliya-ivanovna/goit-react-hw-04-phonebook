64bdac22384f9a7ca88a6f6f8499fbe4300b284e
Liliya-ivanovna <lilyasviatenko@ukr.net>
2023-08-03 19:59:44 +0300

    update

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -3,71 +3,69 @@ import { Button, Label, Form, InputName } from './ContactForm.styled';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 
-export const ContactForm=()=>{
-   const [name, setName]= useState('');
-   const [number, setNumber]= useState('');
+export const ContactForm = ({ createUser }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
   const onHandleChange = e => {
     const { name, value } = e.target;
-   switch(name){
-    case 'name':
-      setName(value);
-      break;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
       case 'number':
         setNumber(value);
         break;
 
-        default:
-           return;
-   }
+      default:
+        return;
+    }
   };
 
-  const onHandleSubmit = (e,{createUser}) => {
+  const onHandleSubmit = e => {
     e.preventDefault();
-    const id = nanoid(2);
-    createUser( {name, number, id});
-   setName('');
-   setNumber('');
-    // this.setState({
-    //   name: '',
-    //   number: '',
-    //});
-  };
-  
-    return (
-      <>
-        <Form onSubmit={onHandleSubmit}>
-          <Label>
-            Name
-            <InputName
-              onChange={onHandleChange}
-              type="text"
-              name="name"
-              value={name}
-              pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-            />
-          </Label>
 
-          <Label>
-            Number
-            <InputName
-              onChange={onHandleChange}
-              type="tel"
-              name="number"
-              value={number}
-              pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-            />{' '}
-          </Label>
-          <Button type="submit">Add contact</Button>
-        </Form>
-      </>
-    );
-  }
+    const id = nanoid(5);
+    createUser({ id, name, number });
+
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <>
+      <Form onSubmit={onHandleSubmit}>
+        <Label>
+          Name
+          <InputName
+            onChange={onHandleChange}
+            type="text"
+            name="name"
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+        </Label>
+
+        <Label>
+          Number
+          <InputName
+            onChange={onHandleChange}
+            type="tel"
+            name="number"
+            value={number}
+            pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />{' '}
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Form>
+    </>
+  );
+};
 
 ContactForm.propTypes = {
   createUser: PropTypes.func.isRequired,
-  };
+};
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,19 +1,18 @@
-import{Component} from "react";
 import PropTypes from 'prop-types';
-import {Input,Label} from './Filter.styled'
+import { Input, Label } from './Filter.styled';
 
-export class Filter extends Component{
-    static propTypes = {
-        addFilter: PropTypes.func.isRequired,
-        filter: PropTypes.string,
-      };
+export const Filter = ({ filter, addFilter }) => {
+  return (
+    <>
+      <Label>
+        Find contacts by name{' '}
+        <Input type="text" name="filter" value={filter} onChange={addFilter} />
+      </Label>
+    </>
+  );
+};
 
-      render(){
-    const { filter, addFilter } = this.props;
-    return(<><Label>Find contacts by name <Input type="text" 
-        name="filter"
-        value={filter}
-        onChange={addFilter}
-        /></Label></>
-    )}
-}
+Filter.propTypes = {
+  addFilter: PropTypes.func.isRequired,
+  filter: PropTypes.string,
+};
